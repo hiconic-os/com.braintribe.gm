312ae7ec35481d3097b3aee219d10132af34bb64
Peter Gazdik <peter.gazdik@gmail.com>
2024-06-11 20:26:21 +0200

    Unmarking generic iface as functional

--- service-api/src/com/braintribe/model/processing/service/impl/DispatchConfiguration.java
+++ service-api/src/com/braintribe/model/processing/service/impl/DispatchConfiguration.java
@@ -16,7 +16,6 @@ import com.braintribe.model.processing.service.api.ReasonedServiceProcessor;
 import com.braintribe.model.processing.service.api.ServiceProcessor;
 import com.braintribe.model.service.api.ServiceRequest;
 
-@FunctionalInterface
 public interface DispatchConfiguration<P extends ServiceRequest, R> {
 	<T extends P> void register(EntityType<T> denotationType, ServiceProcessor<T, ? extends R> processor);
 
