6c3d4acc539f389c60d7e180fdcb8669653a3fe9
Peter Gazdik <peter.gazdik@gmail.com>
2024-07-03 11:19:57 +0200

    Fixing Apache License

--- essential-meta-data-model/src/com/braintribe/model/meta/data/constraint/FileName.java
+++ essential-meta-data-model/src/com/braintribe/model/meta/data/constraint/FileName.java
@@ -11,7 +11,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 // ============================================================================
-// ============================================================================
 package com.braintribe.model.meta.data.constraint;
 
 import com.braintribe.model.generic.annotation.meta.Description;
