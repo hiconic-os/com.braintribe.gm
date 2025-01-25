f2593f5f71b5d02d5e17138832a7c8c45f3b528c
Peter Gazdik <peter.gazdik@gmail.com>
2025-01-25 12:46:07 +0100

    Fixing MappingSPDispatching

--- service-api/src/com/braintribe/model/processing/service/impl/MappingServiceProcessorDispatching.java
+++ service-api/src/com/braintribe/model/processing/service/impl/MappingServiceProcessorDispatching.java
@@ -28,13 +28,13 @@ import com.braintribe.model.processing.service.api.ServiceRequestContext;
 import com.braintribe.model.service.api.ServiceRequest;
 
 public class MappingServiceProcessorDispatching<P extends ServiceRequest, R> implements DispatchConfigurator<P, R> {
-	
-	private MappingServiceProcessor<P, R> annotatedServiceProcessor;
+
+	private final MappingServiceProcessor<P, R> annotatedServiceProcessor;
 
 	static <P extends ServiceRequest, R> MappingServiceProcessorDispatching<P, R> create(MappingServiceProcessor<P, R> persistenceProcessor) {
 		return new MappingServiceProcessorDispatching<>(persistenceProcessor);
 	}
-	
+
 	public MappingServiceProcessorDispatching(MappingServiceProcessor<P, R> persistenceProcessor) {
 		this.annotatedServiceProcessor = persistenceProcessor;
 	}
@@ -42,74 +42,76 @@ public class MappingServiceProcessorDispatching<P extends ServiceRequest, R> imp
 	@Override
 	public void configureDispatching(DispatchConfiguration<P, R> dispatching) {
 		Lookup lookup = MethodHandles.lookup();
-		for (Method method: annotatedServiceProcessor.getClass().getMethods()) {
+		for (Method method : annotatedServiceProcessor.getClass().getMethods()) {
 			Service serviceAnnotation = method.getAnnotation(Service.class);
-			
+
 			if (serviceAnnotation == null)
 				continue;
-			
-			
+
 			Pair<EntityType<P>, SignatureType> signature = checkSignature(method);
 			EntityType<P> requestType = signature.first();
 			SignatureType signatureType = signature.second();
-			
+
 			try {
 				MethodHandle methodHandle = lookup.unreflect(method);
 				MethodHandle boundHandle = methodHandle.bindTo(annotatedServiceProcessor);
-				
-		        if (method.getReturnType() == Maybe.class)
-		        	dispatching.register(requestType, new MethodHandleServiceProcessor<>(boundHandle, signatureType));
-		        else
-		        	dispatching.registerReasoned(requestType, new ReasonedMethodHandleServiceProcessor<>(boundHandle, signatureType));
-			}
-			catch (Error e) {
+
+				if (method.getReturnType() == Maybe.class)
+					dispatching.registerReasoned(requestType, new ReasonedMethodHandleServiceProcessor<>(boundHandle, signatureType));
+				else
+					dispatching.register(requestType, new MethodHandleServiceProcessor<>(boundHandle, signatureType));
+
+			} catch (Error e) {
 				throw e;
-			}
-			catch (Throwable e) {
-				throw new IllegalStateException("Unexpected Exception when converting Transaction annotated method to PersistenceServiceProcessor", e);
+
+			} catch (Throwable e) {
+				throw new IllegalStateException("Unexpected Exception when converting @Service annotated method to ServiceProcessor", e);
 			}
 		}
 	}
-	
+
 	private Pair<EntityType<P>, SignatureType> checkSignature(Method method) {
 		Class<?>[] parameterTypes = method.getParameterTypes();
 
 		final Class<?> requestParameterType;
 		final SignatureType signatureType;
-		
+
 		switch (parameterTypes.length) {
 			case 1:
-				requestParameterType = parameterTypes[1];
+				requestParameterType = parameterTypes[0];
 				signatureType = SignatureType.REQUEST;
 				break;
-				
+
 			case 2: {
 				Class<?> arg1Type = parameterTypes[0];
 				Class<?> arg2Type = parameterTypes[1];
-	
+
 				if (arg1Type == ServiceRequestContext.class) {
 					requestParameterType = arg2Type;
 					signatureType = SignatureType.CONTEXT_REQUEST;
-				}
-				else if (arg2Type == ServiceRequestContext.class) {
+
+				} else if (arg2Type == ServiceRequestContext.class) {
 					requestParameterType = arg1Type;
 					signatureType = SignatureType.REQUEST_CONTEXT;
+
+				} else {
+					throw new UnsupportedOperationException(
+							"method " + method + " is annotated with @Service but does not comply with any of the supported parameter signatures");
 				}
-				else 
-					throw new UnsupportedOperationException("method " + method + " is annotated with @Service but does not comply with any of the supported parameter signatures");
-					
+
 				break;
 			}
 			default:
-				throw new UnsupportedOperationException("method " + method + " is annotated with @Service but does not comply with any of the supported parameter signatures");
+				throw new UnsupportedOperationException(
+						"method " + method + " is annotated with @Service but does not comply with any of the supported parameter signatures");
 		}
 
 		if (!ServiceRequest.class.isAssignableFrom(requestParameterType))
-			throw new UnsupportedOperationException("method " + method + " is annotated with @Service but does not comply with any of the supported parameter signatures");
-		
-		
+			throw new UnsupportedOperationException(
+					"method " + method + " is annotated with @Service but does not comply with any of the supported parameter signatures");
+
 		EntityType<P> requestType = EntityTypes.T((Class<P>) requestParameterType);
-		
+
 		return Pair.of(requestType, signatureType);
 	}
 }
