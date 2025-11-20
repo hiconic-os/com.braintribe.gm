049d18a23070235c13433c3e6efc49b6028f518d
TheConfusedCat <roman@auxilii.com>
2025-11-20 20:53:00 +0100

    Added a limit for fetching

--- graph-fetching/src/com/braintribe/gm/graphfetching/processing/fetch/ToOneRecursiveFetching.java
+++ graph-fetching/src/com/braintribe/gm/graphfetching/processing/fetch/ToOneRecursiveFetching.java
@@ -49,7 +49,7 @@ public class ToOneRecursiveFetching {
 			this.pos = source.pos();
 			this.source = source;
 		}
-		
+
 		public void addJoinable(EntityGraphNode subNode) {
 			joinableNodes.add(subNode);
 		}
@@ -58,15 +58,14 @@ public class ToOneRecursiveFetching {
 			nonJoinableSubTypeNodes.add(subNode);
 		}
 
-
 		public int pos() {
 			return pos;
 		}
-		
+
 		public boolean isRecursionStop() {
 			return recursionStop;
 		}
-		
+
 		public void setRecursionStop(boolean recursionStop) {
 			this.recursionStop = recursionStop;
 		}
@@ -78,7 +77,7 @@ public class ToOneRecursiveFetching {
 		public List<FetchedEntityMapping> wirings() {
 			return wirings;
 		}
-		
+
 		public abstract EntityType<?> entityType();
 	}
 
@@ -89,17 +88,17 @@ public class ToOneRecursiveFetching {
 		public ExistingEntityMapping(AbstractEntityGraphNode node, FetchSource source) {
 			super(source);
 			this.node = node;
-			
+
 			EntityType<?> baseType = node.entityType();
-			
-			for (EntityGraphNode entityNode: node.entityNodes()) {
+
+			for (EntityGraphNode entityNode : node.entityNodes()) {
 				if (supportsSubTypeJoin || entityNode.entityType() == baseType)
 					addJoinable(entityNode);
 				else
 					addNonJoinable(entityNode);
 			}
 		}
-		
+
 		@Override
 		public EntityType<?> entityType() {
 			return node.entityType();
@@ -119,7 +118,7 @@ public class ToOneRecursiveFetching {
 		public Property property() {
 			return property;
 		}
-		
+
 		@Override
 		public EntityType<?> entityType() {
 			return entityType;
@@ -130,38 +129,38 @@ public class ToOneRecursiveFetching {
 	private final FetchQuery fetchQuery;
 	private final boolean supportsSubTypeJoin;
 	private final AbstractEntityGraphNode rootNode;
-	private final ExistingEntityMapping rootMapping; 
+	private final ExistingEntityMapping rootMapping;
 
 	public ToOneRecursiveFetching(FetchContext context, AbstractEntityGraphNode node) {
 		rootNode = node;
 		CyclicRecurrenceCheck check = new CyclicRecurrenceCheck();
-		
+
 		FetchQueryFactory queryFactory = context.queryFactory();
-		
+
 		supportsSubTypeJoin = queryFactory.supportsSubTypeJoin();
-		
+
 		fetchQuery = queryFactory.createQuery(node.entityType(), context.session().getAccessId());
-		
+
 		FetchSource from = fetchQuery.from();
 		rootMapping = new ExistingEntityMapping(node, from);
-		
+
 		registerMappingWithJoins(rootMapping, check);
 	}
 
 	public void fetch(FetchContext context, FetchTask task) {
 		// if there is no sub type joining we need to enqueue subtype nodes specifically for TO_ONE
 		if (!supportsSubTypeJoin) {
-			for (EntityGraphNode entityNode: rootNode.entityNodes()) {
+			for (EntityGraphNode entityNode : rootNode.entityNodes()) {
 				EntityType<?> entityType = entityNode.entityType();
 				if (entityType == rootNode.entityType())
 					continue;
 
 				// enqueue non joinable for TO_ONE
 				Map<Object, GenericEntity> enqueuedEntities = new LinkedHashMap<>();
-				for (GenericEntity entity: task.entities.values()) {
+				for (GenericEntity entity : task.entities.values()) {
 					if (!entityType.isInstance(entity))
 						continue;
-					
+
 					EntityIdm entityIdm = context.acquireEntity(entity);
 					if (entityIdm.addHandled(entityNode.toOneQualification())) {
 						enqueuedEntities.put(entity.getId(), entity);
@@ -170,17 +169,20 @@ public class ToOneRecursiveFetching {
 				context.enqueueToOneIfRequired(entityNode, enqueuedEntities);
 			}
 		}
-		
+
+		if (mappings.size() == 1) {
+			return;
+		}
+
 		ResultWiringContext wiringContext = new ResultWiringContext(context, task.entities);
-		
-		
+
 		Set<Object> allIds = task.entities.keySet();
 
 		// query joined entities in bulks
 		List<Set<Object>> idBulks = CollectionTools2.splitToSets(allIds, context.bulkSize());
 
 		long nanoStart = System.nanoTime();
-		
+
 		context.processParallel(idBulks, ids -> {
 			FetchResults results = fetchQuery.fetchFor(ids);
 			wiringContext.handleRows(results);
@@ -198,49 +200,58 @@ public class ToOneRecursiveFetching {
 			context.enqueueToOneIfRequired(entry.getKey(), entry.getValue());
 		}
 	}
-	
-	/** This method handles the following cases in the following ways: 
 
-		<h1>1. no poly node</h2>
-		
-			<p><b>Note:</b> In that case there would be either exactly one joinable node or exactly one non-joinable subtype node as there is only one entity node attachable to the property node</p>
-			
-			<ul>
-			<li>enqueue TO_MANY for joinable nodes
-			<li>enqueue TO_MANY for non-joinable subtype nodes 
-			<li>enqueue TO_ONE for non-joinable subtype nodes
-			</ul>
-		<h1>2. poly node with non-joinable nodes </h1>
-			
-			<p><b>Note:</b> In that case there can be multiple joinable nodes and multiple non-joinable subtype nodes as a poly node can hold many entity nodes of both kinds</p>
-			<ul>
-			<li>mark TO_ONE for the poly node
-			<li>enqueue TO_MANY for poly node
-			<li>mark TO_MANY for joinable nodes
-			<li>mark TO_MANY for non-joinable subtype nodes 
-			<li>enqueue TO_ONE for non-joinable subtype nodes
-			</ul>
-			
-		<h1>2. poly node without non-joinable nodes</h2>
-			
-			<p><b>Note:</b> In that case there can be multiple joinable nodes and no non-joinable subtype nodes as the cast support can join all types</p>
-			
-			<ul>
-			<li>enqueue TO_ONE for the poly node
-			<li>enqueue TO_MANY for the poly node
-			<li>mark TO_ONE for joinable nodes
-			<li>mark TO_MANY for joinable nodes
-			</ul>
+	/**
+	 * This method handles the following cases in the following ways:
+	 * 
+	 * <h1>1. no poly node</h2>
+	 * 
+	 * <p>
+	 * <b>Note:</b> In that case there would be either exactly one joinable node or exactly one non-joinable subtype node as there is only one entity
+	 * node attachable to the property node
+	 * </p>
+	 * 
+	 * <ul>
+	 * <li>enqueue TO_MANY for joinable nodes
+	 * <li>enqueue TO_MANY for non-joinable subtype nodes
+	 * <li>enqueue TO_ONE for non-joinable subtype nodes
+	 * </ul>
+	 * <h1>2. poly node with non-joinable nodes</h1>
+	 * 
+	 * <p>
+	 * <b>Note:</b> In that case there can be multiple joinable nodes and multiple non-joinable subtype nodes as a poly node can hold many entity
+	 * nodes of both kinds
+	 * </p>
+	 * <ul>
+	 * <li>mark TO_ONE for the poly node
+	 * <li>enqueue TO_MANY for poly node
+	 * <li>mark TO_MANY for joinable nodes
+	 * <li>mark TO_MANY for non-joinable subtype nodes
+	 * <li>enqueue TO_ONE for non-joinable subtype nodes
+	 * </ul>
+	 * 
+	 * <h1>2. poly node without non-joinable nodes</h2>
+	 * 
+	 * <p>
+	 * <b>Note:</b> In that case there can be multiple joinable nodes and no non-joinable subtype nodes as the cast support can join all types
+	 * </p>
+	 * 
+	 * <ul>
+	 * <li>enqueue TO_ONE for the poly node
+	 * <li>enqueue TO_MANY for the poly node
+	 * <li>mark TO_ONE for joinable nodes
+	 * <li>mark TO_MANY for joinable nodes
+	 * </ul>
 	 */
 
 	private List<NodePostProcessing> buildNodePostProcessings(PostProcessing postProcessing, EntityMapping mapping) {
 		List<NodePostProcessing> processings = new ArrayList<>();
-		
+
 		final boolean toOneRecursionStop = mapping.isRecursionStop();
-		
+
 		final boolean withPolyNode = mapping.possiblePolyNode != null;
 		final boolean withNonJoinableNodes = !mapping.nonJoinableSubTypeNodes.isEmpty();
-		
+
 		final boolean enqueuePolyToOne;
 		final boolean enqueuePolyToMany;
 		final boolean enqueueJoinableToOne;
@@ -255,16 +266,14 @@ public class ToOneRecursiveFetching {
 			enqueueJoinableToMany = true;
 			enqueueNonJoinableToOne = true;
 			enqueueNonJoinableToMany = true;
-		}
-		else if (withNonJoinableNodes) {
+		} else if (withNonJoinableNodes) {
 			enqueuePolyToOne = false;
 			enqueuePolyToMany = true;
 			enqueueJoinableToOne = toOneRecursionStop;
 			enqueueJoinableToMany = false;
 			enqueueNonJoinableToOne = true;
 			enqueueNonJoinableToMany = false;
-		}
-		else {
+		} else {
 			enqueuePolyToOne = toOneRecursionStop;
 			enqueuePolyToMany = true;
 			enqueueJoinableToOne = false;
@@ -272,10 +281,10 @@ public class ToOneRecursiveFetching {
 			enqueueNonJoinableToOne = false;
 			enqueueNonJoinableToMany = false;
 		}
-		
-		if (withPolyNode) 
+
+		if (withPolyNode)
 			processings.add(new NodePostProcessing(postProcessing, mapping.possiblePolyNode, enqueuePolyToOne, enqueuePolyToMany, false));
-		
+
 		for (EntityGraphNode node : mapping.joinableNodes) {
 			boolean subTypeNode = mapping.property.getType() != node.entityType();
 			processings.add(new NodePostProcessing(postProcessing, node, enqueueJoinableToOne, enqueueJoinableToMany, subTypeNode));
@@ -307,7 +316,8 @@ public class ToOneRecursiveFetching {
 		private final boolean enqueueToMany;
 		private final boolean enqueueToOne;
 
-		public NodePostProcessing(PostProcessing postProcessing, AbstractEntityGraphNode node, boolean enqueueToOne, boolean enqueueToMany, boolean isSubTypeNode) {
+		public NodePostProcessing(PostProcessing postProcessing, AbstractEntityGraphNode node, boolean enqueueToOne, boolean enqueueToMany,
+				boolean isSubTypeNode) {
 			super();
 			this.node = node;
 			this.enqueueToOne = enqueueToOne;
@@ -319,13 +329,13 @@ public class ToOneRecursiveFetching {
 
 		public void handle(EntityIdm entityIdm) {
 			GenericEntity entity = entityIdm.entity;
-			
+
 			if (isSubTypeNode && !node.entityType().isInstance(entity))
 				return;
 
 			if (entityIdm.addHandled(node.toOneQualification()) && enqueueToOne)
 				toOnes.put(entity.getId(), entity);
-			
+
 			if (entityIdm.addHandled(node.toManyQualification()) && enqueueToMany)
 				toManies.put(entity.getId(), entity);
 		}
@@ -334,63 +344,64 @@ public class ToOneRecursiveFetching {
 	private static class CyclicRecurrenceCheck {
 		private Map<Property, AtomicInteger> recurrences = new IdentityHashMap<>();
 		private int limit = 3;
-		
+
 		public boolean add(Property property) {
 			AtomicInteger counter = recurrences.computeIfAbsent(property, k -> new AtomicInteger(0));
-			
+
 			counter.incrementAndGet();
-			
+
 			return counter.get() > limit;
 		}
-		
+
 		public void remove(Property property) {
 			recurrences.compute(property, (n, c) -> {
 				if (c == null || c.get() == 0)
 					throw new IllegalStateException("unexpected remove");
-				
+
 				int counter = c.decrementAndGet();
-				
-				return counter == 0? null: c;
+
+				return counter == 0 ? null : c;
 			});
 		}
 	}
-	
+
 	/**
 	 * Resolves/assigns entity/graph nodes recursively; creates mappings for joined properties.
 	 */
 	private void registerMappingWithJoins(EntityMapping refererMapping, CyclicRecurrenceCheck check) {
 		mappings.add(refererMapping);
 
-		boolean recursionStop = check.add(refererMapping.property);
-		
+		boolean mappingsSizeExceeded = mappings.size() > 20;
+		boolean recursionStop = check.add(refererMapping.property) || mappingsSizeExceeded;
+
 		try {
 			refererMapping.setRecursionStop(recursionStop);
-			
+
 			if (recursionStop)
 				return;
-	
+
 			List<FetchedEntityMapping> newMappings = new ArrayList<>(refererMapping.joinableNodes.size());
-			
+
 			for (EntityGraphNode joinableNode : refererMapping.joinableNodes) {
 				boolean subTypeJoin = refererMapping.entityType() != joinableNode.entityType();
-				
+
 				for (EntityPropertyGraphNode propertyNode : joinableNode.entityProperties().values()) {
 					Property property = propertyNode.property();
-					
+
 					FetchSource fetchSource = refererMapping.source();
 
 					// cast if necessary
 					if (subTypeJoin)
 						fetchSource = fetchSource.as(joinableNode.entityType());
-					
+
 					FetchSource join = fetchSource.leftJoin(property);
 					FetchedEntityMapping fetchedMapping = new FetchedEntityMapping(property, join);
-					
+
 					AbstractEntityGraphNode abstractEntityNode = propertyNode.entityNode();
-					
+
 					fetchedMapping.possiblePolyNode = abstractEntityNode.isPolymorphic();
-					
-					for (EntityGraphNode entityNode: abstractEntityNode.entityNodes()) {
+
+					for (EntityGraphNode entityNode : abstractEntityNode.entityNodes()) {
 						// are only base type properties joinable or also polymorphic ones?
 						if (supportsSubTypeJoin || entityNode.entityType() == property.getType()) {
 							fetchedMapping.addJoinable(entityNode);
@@ -399,17 +410,16 @@ public class ToOneRecursiveFetching {
 							fetchedMapping.addNonJoinable(entityNode);
 						}
 					}
-					
+
 					newMappings.add(fetchedMapping);
 				}
 			}
-	
+
 			// register next level
 			for (FetchedEntityMapping fetchedMapping : newMappings) {
 				registerMappingWithJoins(fetchedMapping, check);
 			}
-		}
-		finally {
+		} finally {
 			check.remove(refererMapping.property);
 		}
 	}
@@ -417,13 +427,14 @@ public class ToOneRecursiveFetching {
 	private static class MappingWithPostProcessing {
 		public final EntityMapping mapping;
 		public final List<NodePostProcessing> postProcessings;
+
 		public MappingWithPostProcessing(EntityMapping mapping, List<NodePostProcessing> postProcessings) {
 			super();
 			this.mapping = mapping;
 			this.postProcessings = postProcessings;
 		}
 	}
-	
+
 	private class ResultWiringContext {
 		public final FetchContext fetchContext;
 		public Map<Object, GenericEntity> existingEntitiesById = new HashMap<Object, GenericEntity>();
@@ -439,41 +450,41 @@ public class ToOneRecursiveFetching {
 				postProcessings.add(new MappingWithPostProcessing(mapping, buildNodePostProcessings(postProcessing, mapping)));
 			}
 		}
-		
+
 		public void handleRows(FetchResults results) {
 			GenericEntity entities[] = new GenericEntity[mappings.size()];
-			
+
 			while (results.next()) {
 				// fill current entities with identity managed entities
 				Object id = results.get(0);
 				GenericEntity rootEntity = existingEntitiesById.get(id);
-		
+
 				entities[0] = rootEntity;
-		
-				for (MappingWithPostProcessing entry: postProcessings) {
+
+				for (MappingWithPostProcessing entry : postProcessings) {
 					EntityMapping mapping = entry.mapping;
 					int col = mapping.pos();
-		
-					GenericEntity entity = (GenericEntity)results.get(col);
-		
+
+					GenericEntity entity = (GenericEntity) results.get(col);
+
 					if (entity == null) {
 						entities[col] = null;
 						continue;
 					}
-		
+
 					EntityIdm entityIdm = fetchContext.acquireEntity(entity);
 					entity = entityIdm.entity;
-					
+
 					entities[col] = entity;
-					
+
 					for (NodePostProcessing nodePostProcessing : entry.postProcessings)
 						nodePostProcessing.handle(entityIdm);
 				}
-				
+
 				wireRow(entities);
 			}
 		}
-		
+
 		/**
 		 * Wires each fetched row into the actual entity instances according to the mapping tree.
 		 */
@@ -482,18 +493,18 @@ public class ToOneRecursiveFetching {
 
 			wireRow(entities, rootMapping, entity);
 		}
-		
+
 		private void wireRow(GenericEntity entities[], EntityMapping mapping, GenericEntity entity) {
-			for (FetchedEntityMapping wiring: mapping.wirings()) {
+			for (FetchedEntityMapping wiring : mapping.wirings()) {
 				GenericEntity wiredEntity = entities[wiring.pos()];
 				Property property = wiring.property();
-				
+
 				if (wiredEntity != null || property.getDeclaringType().isInstance(entity))
 					property.set(entity, wiredEntity);
-				
+
 				if (wiredEntity != null)
 					wireRow(entities, wiring, wiredEntity);
 			}
-		}		
+		}
 	}
 }
