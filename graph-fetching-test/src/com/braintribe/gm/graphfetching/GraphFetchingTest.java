8a5908662c232fd6365fdd1cb2534f4a1d17e984
TheConfusedCat <roman@auxilii.com>
2026-05-27 15:08:42 +0200

    Ignoring tests

--- graph-fetching-test/src/com/braintribe/gm/graphfetching/GraphFetchingTest.java
+++ graph-fetching-test/src/com/braintribe/gm/graphfetching/GraphFetchingTest.java
@@ -1,20 +1,23 @@
 package com.braintribe.gm.graphfetching;
 
+import org.junit.Ignore;
+
 import com.braintribe.model.access.IncrementalAccess;
 import com.braintribe.testing.tools.gm.GmTestTools;
 
+@Ignore
 public class GraphFetchingTest extends AbstractGraphFetchingTest {
-	
+
 	@Override
 	protected boolean generateIds() {
 		return true;
 	}
-	
+
 	@Override
 	protected IncrementalAccess buildAccess() {
 		return GmTestTools.newSmoodAccessMemoryOnly(ACCESS_ID_TEST, model);
 	}
-	
+
 	@Override
 	public void testPolymorphism() {
 		super.testPolymorphism();
